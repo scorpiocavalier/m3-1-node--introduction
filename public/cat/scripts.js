ddffa5f30565734c40397c42f16959d0b0202d24
Arturo Chen <art.can.fly@gmail.com>
2020-07-02 10:24:34 -0400

    1.1-1.2.2 done

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,24 @@
-// here there be JS, yarrr ☠️
+const messageInput = document.querySelector('#user-input');
+const conversationElem = document.querySelector('#conversation-container')
+
+const sendMessage = (event) => {
+  // prevent the default "page reload" from occurring.
+  event.preventDefault();
+
+  const message = {
+    author: 'user',
+    text: messageInput.value
+  }
+
+  updateConversation(message)
+};
+
+const updateConversation = message => {
+  const { author, text } = message
+
+  const messageElem = document.createElement('p')
+
+  messageElem.innerHTML = `<span>${text}</span>`
+
+  conversationElem.appendChild(messageElem)
+}
